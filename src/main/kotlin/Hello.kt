72a283aab4a16d654437e67500913d7edda2a7af
Altair <adaltair03@gmail.com>
2022-09-14 16:08:53 +0600

    2.1

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -3,5 +3,8 @@
 //}
 
 fun main(args: Array<String>) {
-    println("Hello, ${args[0]}")
+//    println("Hello, ${args[0]}")
+    val isUnit = println("This is an expression")
+    println(isUnit)
 }
+
