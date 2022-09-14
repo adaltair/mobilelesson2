3b6f9c40e3ff277908f72f934a0effd88f0f0110
Altair <adaltair03@gmail.com>
2022-09-14 16:09:37 +0600

    2.4

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -4,7 +4,10 @@
 
 fun main(args: Array<String>) {
 //    println("Hello, ${args[0]}")
-    val isUnit = println("This is an expression")
-    println(isUnit)
+//    val isUnit = println("This is an expression")
+//    println(isUnit)
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
 }
 
