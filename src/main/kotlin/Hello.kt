313558c27d8476cf2e7b32585f0952307ab69d1b
Altair <adaltair03@gmail.com>
2022-09-14 16:12:44 +0600

    3.1

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 //fun main(args: Array<String>) {
 //    println("Hello, world!")
 //}
@@ -12,8 +14,19 @@ fun main(args: Array<String>) {
 //    val isHot = if (temperature > 50) true else false
 //    println(isHot)
 
-    val temperature = 10
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
-    println(message)
-}
+//    val temperature = 10
+//    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+//    println(message)
 
+    feedTheFish()
+}
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println ("Today is $day and the fish eat $food")
+}
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
