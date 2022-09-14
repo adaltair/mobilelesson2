9a9be25e59f7c4a27849cd9d171094727f6019f6
Altair <adaltair03@gmail.com>
2022-09-14 16:33:40 +0600

    6.1

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -77,34 +77,40 @@ import java.util.*
 //fun isSunday(day: String) = day == "Sunday"
 val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 fun main() {
-    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+//    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+//
+//    // eager, creates a new list
+//    val eager = decorations.filter { it[0] == 'p' }
+//    println("eager: $eager")
+//    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+//    println("filtered: $filtered")
+//    val newList = filtered.toList()
+//    println("new list: $newList")
+//    val lazyMap = decorations.asSequence().map {
+//        println("access: $it")
+//        it
+//    }
+//    println("lazy: $lazyMap")
+//    println("-----")
+//    println("first: ${lazyMap.first()}")
+//    println("-----")
+//    println("all: ${lazyMap.toList()}")
+//    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+//        println("access: $it")
+//        it
+//    }
+//    println("-----")
+//    println("filtered: ${lazyMap2.toList()}")
+//    val mysports = listOf("basketball", "fishing", "running")
+//    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+//    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+//    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+//    println("-----")
+//    println("Flat: ${mylist.flatten()}")
 
-    // eager, creates a new list
-    val eager = decorations.filter { it[0] == 'p' }
-    println("eager: $eager")
-    val filtered = decorations.asSequence().filter { it[0] == 'p' }
-    println("filtered: $filtered")
-    val newList = filtered.toList()
-    println("new list: $newList")
-    val lazyMap = decorations.asSequence().map {
-        println("access: $it")
-        it
-    }
-    println("lazy: $lazyMap")
-    println("-----")
-    println("first: ${lazyMap.first()}")
-    println("-----")
-    println("all: ${lazyMap.toList()}")
-    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
-        println("access: $it")
-        it
-    }
-    println("-----")
-    println("filtered: ${lazyMap2.toList()}")
-    val mysports = listOf("basketball", "fishing", "running")
-    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
-    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
-    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
-    println("-----")
-    println("Flat: ${mylist.flatten()}")
+    var dirtyLevel = 20
+//    val waterFilter = { dirty : Int -> dirty / 2}
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+    println(waterFilter(dirtyLevel))
+
 }
