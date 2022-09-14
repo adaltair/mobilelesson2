b347165ed899c950076f99d401e139bd51a7a064
Altair <adaltair03@gmail.com>
2022-09-14 16:13:44 +0600

    3.2.1

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -20,13 +20,34 @@ fun main(args: Array<String>) {
 
     feedTheFish()
 }
-fun feedTheFish() {
-    val day = randomDay()
-    val food = "pellets"
-    println ("Today is $day and the fish eat $food")
-}
+//fun feedTheFish() {
+//    val day = randomDay()
+//    val food = "pellets"
+//    println ("Today is $day and the fish eat $food")
+//}
 fun randomDay() : String {
     val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
         "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day : String) : String {
+    var food = ""
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+
+    println ("Today is $day and the fish eat $food")
 }
