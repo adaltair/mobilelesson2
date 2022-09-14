bdbbf1fef558d2b8225905cfa64339bf58124158
Altair <adaltair03@gmail.com>
2022-09-14 16:23:25 +0600

    4.1

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -17,34 +17,40 @@ fun main(args: Array<String>) {
 //    val temperature = 10
 //    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
 //    println(message)
+    swim()   // uses default speed
+    swim("slow")   // positional argument
+    swim(speed="turtle-like")   // named parameter
 
-    feedTheFish()
 }
 //fun feedTheFish() {
 //    val day = randomDay()
 //    val food = "pellets"
 //    println ("Today is $day and the fish eat $food")
 //}
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
-}
+//fun randomDay() : String {
+//    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+//        "Friday", "Saturday", "Sunday")
+//    return week[Random().nextInt(week.size)]
+//}
+//
+//fun fishFood (day : String) : String {
+//    return when (day) {
+//        "Monday" -> "flakes"
+//        "Wednesday" -> "redworms"
+//        "Thursday" -> "granules"
+//        "Friday" -> "mosquitoes"
+//        "Sunday" -> "plankton"
+//        else -> "nothing"
+//    }
+//}
+//
+//fun feedTheFish() {
+//    val day = randomDay()
+//    val food = fishFood(day)
+//
+//    println ("Today is $day and the fish eat $food")
+//}
 
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
-    }
-}
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-
-    println ("Today is $day and the fish eat $food")
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
 }
